1f151cde9993e05c0a800e8c940b2561d49352e7
Sophie Yates <sophie.yates@autotrader.co.uk>
2025-03-11 12:15:55 +0000

    Created PlayingCard class

---
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -0,0 +1,13 @@
+package cards;
+
+public class PlayingCard {
+
+    int suit;
+    int faceValue;
+
+    public PlayingCard(int suit, int faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+
+}
