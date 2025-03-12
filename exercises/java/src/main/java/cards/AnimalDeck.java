21f600f2b79b003d284493b89b26e5ddc048097c
Sophie Yates <sophie.yates@autotrader.co.uk>
2025-03-12 11:16:14 +0000

    Made Snap game refer Deck interface rather than AnimalDeck

--- exercises/java/src/main/java/cards/AnimalDeck.java
+++ exercises/java/src/main/java/cards/AnimalDeck.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class AnimalDeck {
+class AnimalDeck implements Deck {
 
     private List<AnimalCard> cards;
 
@@ -16,10 +16,12 @@ class AnimalDeck {
         }
     }
 
+    @Override
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
+    @Override
     public String[] getCards() {
         String[] result = new String[cards.size()];
         for (int i = 0; i < cards.size(); i++) {
@@ -29,6 +31,7 @@ class AnimalDeck {
         return result;
     }
 
+    @Override
     public AnimalCard deal() {
         return cards.remove(0);
     }
---
+++ exercises/java/src/main/java/cards/Deck.java
@@ -0,0 +1,10 @@
+package cards;
+
+public interface Deck {
+
+    void shuffle();
+
+    String[] getCards();
+
+    Card deal();
+}
